68d00b4c6cec512daa9c623e34e761301c941e73
Deep Toaster <ti.42.plus@gmail.com>
2017-01-08 01:58:45 -0800

    added brick breaking

--- breakout.js
+++ breakout.js
@@ -1,27 +1,146 @@
 function Breakout(presenter) {
-	var self = this;
-	var gridWidth;
-	var gridHeight;
-	var paddleRadius;
-	var ballRadius;
-	var interval;
+  const delay = 30;
+  const speed = 10;
+  var self = this;
+  var gridRows;
+  var gridCols;
+  var gridWidth;
+  var gridHeight;
+  var paddleRadius;
+  var ballRadius;
+  var brickWidth;
+  var interval;
+  var v;
+  var w;
+  var x;
+  var y;
+  var paddleX;
 
-	this.load = function(rows, cols) {
-		presenter.model = self;
-		presenter.createGrid(rows, cols);
-		interval = 0;
-	};
+  function loop() {
+    xp = x + v;
+    yp = y + w;
 
-	this.scroll = function(x) {
-		if (!interval) {
-			presenter.moveTo(x, gridHeight - ballRadius);
-		}
-	};
+    if (xp < ballRadius || xp + ballRadius > gridWidth) {
+      v = -v;
+    } else if (yp < ballRadius) {
+      w = -w;
+    } else if (yp + ballRadius > gridHeight) {
+      var theta = (xp - paddleX) / paddleRadius;
 
-	this.setGrid = function(width, height, paddle, ball) {
-		gridWidth = width;
-		gridHeight = height;
-		paddleRadius = paddle;
-		ballRadius = ball;
-	};
+      if (theta > -1 && theta < 1) {
+        theta = theta * Math.PI * 0.4;
+        v = speed * Math.sin(theta);
+        w = -speed * Math.cos(theta);
+      } else {
+        clearInterval(interval);
+        presenter.reset();
+        interval = 0;
+      }
+    } else {
+      var valid = true;
+
+      var j0 = Math.floor((y - ballRadius) / brickHeight);
+      var j1 = Math.floor((y + ballRadius) / brickHeight);
+
+      if (v > 0) {
+        var i = Math.floor((xp + ballRadius) / brickWidth);
+
+        if (i != Math.floor((x + ballRadius) / brickWidth)) {
+          var valid0 = presenter.breakBrick(i, j0);
+          var valid1 = presenter.breakBrick(i, j1);
+
+          if (valid0 || valid1) {
+            v = -v;
+            valid = false;
+          }
+        }
+      } else {
+        var i = Math.floor((xp - ballRadius) / brickWidth);
+
+        if (i != Math.floor((x - ballRadius) / brickWidth)) {
+          var valid0 = presenter.breakBrick(i, j0);
+          var valid1 = presenter.breakBrick(i, j1);
+
+          if (valid0 || valid1) {
+            v = -v;
+            valid = false;
+          }
+        }
+      }
+
+      var i0 = Math.floor((x - ballRadius) / brickWidth);
+      var i1 = Math.floor((x + ballRadius) / brickWidth);
+
+      if (w > 0) {
+        var j = Math.floor((yp + ballRadius) / brickHeight);
+
+        if (j != Math.floor((y + ballRadius) / brickHeight)) {
+          var valid0 = presenter.breakBrick(i0, j);
+          var valid1 = presenter.breakBrick(i1, j);
+
+          if (valid0 || valid1) {
+            w = -w;
+            valid = false;
+          }
+        }
+      } else {
+        var j = Math.floor((yp - ballRadius) / brickHeight);
+
+        if (j != Math.floor((y - ballRadius) / brickHeight)) {
+          var valid0 = presenter.breakBrick(i0, j);
+          var valid1 = presenter.breakBrick(i1, j);
+
+          if (valid0 || valid1) {
+            w = -w;
+            valid = false;
+          }
+        }
+      }
+
+      if (valid) {
+        x = xp;
+        y = yp;
+        presenter.moveTo(x, y);
+      }
+    }
+  }
+
+  this.load = function(rows, cols) {
+    gridRows = rows;
+    gridCols = cols;
+    presenter.model = self;
+    presenter.createGrid(rows, cols);
+    presenter.reset();
+    interval = 0;
+  };
+
+  this.scroll = function(paddle) {
+    paddleX = paddle;
+
+    if (!interval) {
+      x = paddle;
+
+      presenter.moveTo(x, gridHeight - ballRadius);
+    }
+  };
+
+  this.setGrid = function(width, height, brick, paddle, ball) {
+    gridWidth = width;
+    gridHeight = height;
+    paddleRadius = paddle;
+    ballRadius = ball;
+    brickWidth = width / gridCols;
+    brickHeight = brick;
+    y = gridHeight - ballRadius;
+  };
+
+  this.start = function() {
+    if (!interval) {
+      var theta = (Math.random() - 0.5) * Math.PI * 0.8
+
+      v = speed * Math.sin(theta);
+      w = -speed * Math.cos(theta);
+      interval = setInterval(loop, delay);
+    }
+  };
 }
--- breakoutpresenter.js
+++ breakoutpresenter.js
@@ -1,44 +1,66 @@
 function BreakoutPresenter(target) {
-	var self = this;
-	var colors = ['red', 'orange', 'yellow', 'green'];
-	var right;
-	var paddleRadius;
-	var ballRadius;
-	var $grid;
-	var $paddle;
-	var $ball;
+  var self = this;
+  var colors = ['red', 'orange', 'yellow', 'green'];
+  var right;
+  var paddleRadius;
+  var ballRadius;
+  var $grid;
+  var $paddle;
+  var $ball;
 
-	this.createGrid = function(rows, cols) {
-		var ul = '';
+  this.breakBrick = function(i, j) {
+    var $brick = $grid.children().eq(j).children().eq(i);
+    var valid = $brick.length && !$brick.hasClass('breakout-broken');
 
-		for (var i = 0; i < rows; i++) {
-			ul += '<ul class="breakout-' + colors[i] + '">';
+    $brick.addClass('breakout-broken');
+    return valid;
+  }
 
-			for (var j = 0; j < cols; j++) {
-				ul += '<li />';
-			}
+  this.createGrid = function(rows, cols) {
+    var ul = '';
 
-			ul += '</ul>';
-		}
+    for (var i = 0; i < rows; i++) {
+      ul += '<ul class="breakout-' + colors[i] + '">';
 
-		$grid = $('<div class="breakout">' + ul + '</div>').appendTo($(target));
+      for (var j = 0; j < cols; j++) {
+        ul += '<li />';
+      }
 
-		$paddle = $('<div class="breakout-paddle"><span /></div>').scroll(function() {
-			self.model.scroll(($(this).scrollLeft() / right * 4.1 + 0.46) * paddleRadius);
-		}).appendTo($grid);
+      ul += '</ul>';
+    }
 
-		$ball = $('<input type="radio" checked="checked" />').appendTo($grid);
-		paddleRadius = $paddle.width() / 5;
-		right = $paddle.children().width() - $paddle.width();
-		$paddle.scrollLeft(right / 2);
-		ballRadius = ($ball.height() + $ball.width()) / 4;
-		self.model.setGrid($grid.width(), $paddle.position().top, paddleRadius, ballRadius);
-	};
+    $grid = $('<div class="breakout">' + ul + '</div>').appendTo($(target));
 
-	this.moveTo = function(x, y) {
-		$ball.css({
-			top: y - ballRadius,
-			left: x - ballRadius
-		});
-	}
+    $paddle = $('<div class="breakout-paddle"><span /></div>')
+        .scroll(function() {
+      self.model.scroll(
+        ($(this).scrollLeft() / right * 8.8 + 1) * paddleRadius
+      );
+    }).appendTo($grid);
+
+    $ball = $('<input type="radio" checked="checked" />').appendTo($grid);
+    paddleRadius = $paddle.width() * 0.093;
+    right = $paddle.children().width() - $paddle.width();
+    ballRadius = ($ball.height() + $ball.width()) / 4;
+
+    self.model.setGrid(
+      $grid.outerWidth(),
+      $paddle.position().top,
+      $grid.children().outerHeight(),
+      paddleRadius,
+      ballRadius
+    );
+  };
+
+  this.moveTo = function(x, y) {
+    $ball.css({
+      top: y - ballRadius,
+      left: x - ballRadius
+    });
+  }
+
+  this.reset = function() {
+    $paddle.scrollLeft(0);
+    $paddle.scrollLeft(right / 2);
+  }
 }
